605630999ea92f3416bdae177e7546db54194935
Jaime Andres <jaimeandres2998@gmail.com>
2020-10-01 12:27:44 -0500

    docs added

---
+++ docs/precache-manifest.513114ea878f46f425ff026dcc2784a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac86e1e7a05a699bdae8b8913f27e0c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e31e7a1b07a1070ba0",
+    "url": "/static/css/main.7f56302c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e31e7a1b07a1070ba0",
+    "url": "/static/js/main.0f3f2f34.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
